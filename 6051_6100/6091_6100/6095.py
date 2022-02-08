541529a89ed40f67ac788281a7d93d10f0a292c9
rlatmdwn01 <69657433+rlatmdwn01@users.noreply.github.com>
2022-02-08 16:53:55 +0900

    Create 6095.py

---
+++ 6051_6100/6091_6100/6095.py
@@ -0,0 +1,16 @@
+#바둑판모양기본
+d=[]
+for i in range(20):
+  d.append([])
+  for j in range(20):
+    d[i].append(0)
+
+n=int(input())
+for i in range(n):
+  x,y=map(int, input().split())
+  d[int(x)][int(y)]=1
+
+for i in range(1,20):
+  for j in range(1,20):
+    print(d[i][j],end=' ')
+  print()
